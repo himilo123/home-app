104b9284546a308184ce4b9016e8617723ca9e70
Ahmed Suleiman <94101874+himilo123@users.noreply.github.com>
2026-02-11 13:16:46 +0300

    Update server listen method for better accessibility

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,8 @@ app.get('/', (req, res) => {
   res.send('HOME Maintenance API running...');
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Server is live on port ${PORT}`);
+});
